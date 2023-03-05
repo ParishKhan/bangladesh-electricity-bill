2180ceaf534199bc112b6b20cd3e09779e0ff27c
PariSh KhAn <parish@artlist.io>
2023-03-06 01:10:37 +0600

    Update Electricity Bill Chart

--- src/data.ts
+++ src/data.ts
@@ -15,114 +15,114 @@ type bilingType = {
 type rangeType = {
   unit: [number, number?];
   price: number;
-}
+};
 
 const BILLING_INFO = {
   vat: 5,
   lifeLineUnit: 50,
-  lifeLinePrice: 3.75
-}
+  lifeLinePrice: 4.14,
+};
 
 const prices: bilingType = Object.freeze({
   "LT-A": {
     name: "Residential",
-    demand: 30,
+    demand: 35,
     step: true,
     range: [
       {
         unit: [0, 75],
-        price: 4.19,
+        price: 4.62,
       },
       {
         unit: [75, 200],
-        price: 5.72,
+        price: 6.31,
       },
       {
         unit: [200, 300],
-        price: 6.0,
+        price: 6.62,
       },
       {
         unit: [300, 400],
-        price: 6.34,
+        price: 6.99,
       },
       {
         unit: [400, 600],
-        price: 9.94,
+        price: 10.96,
       },
       {
         unit: [600, 99999999999],
-        price: 11.46,
+        price: 12.63,
       },
     ],
   },
   "LT-B": {
     name: "Agricultural pumps",
-    demand: 30,
+    demand: 35,
     step: false,
-    flat: 4.16,
+    flat: 4.59,
     offPeak: null,
     peak: null,
     superOffPeak: null,
   },
   "LT-C1": {
     name: "Retail Industry",
-    demand: 30,
+    demand: 40,
     step: false,
-    flat: 8.53,
-    offPeak: 7.68,
-    peak: 10.24,
+    flat: 9.41,
+    offPeak: 8.46,
+    peak: 11.29,
     superOffPeak: null,
   },
   "LT-C2": {
     name: "Construction",
     demand: 100,
     step: false,
-    flat: 12.0,
+    flat: 13.23,
     offPeak: null,
     peak: null,
     superOffPeak: null,
   },
   "LT-D1": {
     name: "Educational, Religious and Charitable Institutions and Hospitals",
-    demand: 35,
+    demand: 50,
     step: false,
-    flat: 6.02,
+    flat: 6.64,
     offPeak: null,
     peak: null,
     superOffPeak: null,
   },
   "LT-D2": {
     name: "Street lights and water pumps",
-    demand: 60,
+    demand: 75,
     step: false,
-    flat: 7.7,
+    flat: 8.49,
     offPeak: null,
     peak: null,
     superOffPeak: null,
   },
   "LT-D3": {
     name: "Battery charging station",
-    demand: 60,
+    demand: 75,
     step: false,
-    flat: 7.64,
-    offPeak: 6.88,
-    peak: 9.55,
-    superOffPeak: 6.11,
+    flat: 8.42,
+    offPeak: 7.58,
+    peak: 10.53,
+    superOffPeak: 6.74,
   },
   "LT-E": {
     name: "Commercial and Office",
-    demand: 60,
+    demand: 75,
     step: false,
-    flat: 10.3,
-    offPeak: 9.27,
-    peak: 12.36,
+    flat: 11.36,
+    offPeak: 10.22,
+    peak: 13.63,
     superOffPeak: null,
   },
   "LT-T": {
     name: "Temporary",
     demand: 100,
     step: false,
-    flat: 16.0,
+    flat: 17.64,
     offPeak: null,
     peak: null,
     superOffPeak: null,
